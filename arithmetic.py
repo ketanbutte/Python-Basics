c801ee200c80a686b86bacf4443dafc0a73a5f76
ketanbutte <ketanbutte98@gmail.com>
2023-03-23 20:22:23 +0530

    daily practice 1

---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+# print(1+13,'is the answer for addition') #addition
+
+# print(13-2,'is the answer for subtraction') #subtraction
+
+# print(94/45,'is the answer for division') #division
+
+# print(92//45,'is the answer for floor division(quotient)') ##floor division
+
+
+# print(93 % 45 ,'is the answer for modulus operation(remainder)') #modulus operator
+
+# print(2**3, 'is the answer by using power operator')
+
+
+
+##Shortcuts
+
+i = 2
+
+# i = i +2
+i += 3
+print(i)
---
+++ strings.py
@@ -0,0 +1,8 @@
+
+name = "Peter Parker"
+
+# print(name.find("Parker"))
+
+print(name.replace("Peter","Virat"))
+
+print(name)
