4352fc203e68f94847c18a719f2aba451fd8a350
ketanbutte <ketanbutte98@gmail.com>
2023-03-25 04:39:59 +0530

    daily practice 2

---
+++ comparison_operators.py
@@ -0,0 +1,28 @@
+## What are comparison operators?
+#---> e.g equal to,greater than, less than 
+
+
+# print(3>5)
+
+# print(3>=3)
+
+# print(4<=6)
+
+# print(4 == 10)
+
+# print(True or False)
+
+# print(True and False)
+
+# print(True | False )
+
+# print(True & False )
+
+
+customers = 100
+
+revenue = 20000
+
+condition = customers>50 and revenue > 15000
+
+print(condition)
--- operator_precedence.py
+++ operator_precedence.py
@@ -1,3 +1,15 @@
-result = 2 + 3*5
+result = 2**3 + (5/2)**2
 
 print(result)
+
+
+##a short intro on comments
+## we can use # to comment-not part of the code-use for better documentation
+#use to explain the code to your future self-colleagues,friends etc so that they dont have to
+# to have you in person to explain the code
+## write or use comments efficiently to ensure the code is well explained.
+
+"""
+Multiline commenting-typically used for docstrings- docstrings are documentation.
+"""
+
